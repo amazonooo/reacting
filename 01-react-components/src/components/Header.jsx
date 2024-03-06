dc56a1353fdf5ddf4fee0573c9946673f5e0c7b8
amazonooo <garikogannisyan.9@gmail.com>
2024-03-06 18:53:57 +0300

    1

---
+++ 01-react-components/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '/logo-name.svg'
+
+function Header() {
+    const now = new Date()
+	return (
+		<header>
+            <img src={logo} alt="" />
+			{/* <h3>Reuslt University</h3> */}
+
+			<span>Время сейчас: {now.toLocaleTimeString()}</span>
+		</header>
+	)
+}
+
+export default Header
