00253a9bc16dda01b253c716ba5a1bd5074dd1d5
amazonooo <garikogannisyan.9@gmail.com>
2024-03-07 14:28:36 +0300

    fsd

--- 01-react-components-finish/src/components/Button/Button.jsx
+++ 01-react-components-finish/src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
-import classes from './Button.module.css'
+// import classes from './Button.module.css'
 
-export default function Button({ children, isActive, ...props }) {
-  return (
-    <button
-      {...props}
-      className={
-        isActive ? `${classes.button} ${classes.active}` : classes.button
-      }
-    >
-      {children}
-    </button>
-  )
-}
+// export default function Button({ children, isActive, ...props }) {
+//   return (
+//     <button
+//       {...props}
+//       className={
+//         isActive ? `${classes.button} ${classes.active}` : classes.button
+//       }
+//     >
+//       {children}
+//     </button>
+//   )
+// }
--- 01-react-components-finish/src/components/Modal/Modal.jsx
+++ 01-react-components-finish/src/components/Modal/Modal.jsx
@@ -1,20 +1,20 @@
-import { createPortal } from 'react-dom'
-import { useRef, useEffect } from 'react'
-import './Modal.css'
+// import { createPortal } from 'react-dom'
+// import { useRef, useEffect } from 'react'
+// import './Modal.css'
 
-export default function Modal({ children, open }) {
-  const dialog = useRef()
+// export default function Modal({ children, open }) {
+//   const dialog = useRef()
 
-  useEffect(() => {
-    if (open) {
-      dialog.current.showModal()
-    } else {
-      dialog.current.close()
-    }
-  }, [open])
+//   useEffect(() => {
+//     if (open) {
+//       dialog.current.showModal()
+//     } else {
+//       dialog.current.close()
+//     }
+//   }, [open])
 
-  return createPortal(
-    <dialog ref={dialog}>{children}</dialog>,
-    document.getElementById('modal')
-  )
-}
+//   return createPortal(
+//     <dialog ref={dialog}>{children}</dialog>,
+//     document.getElementById('modal')
+//   )
+// }
