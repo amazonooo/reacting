969bee270e9c14f333deef280451e84672d785bf
amazonooo <garikogannisyan.9@gmail.com>
2024-03-09 20:41:13 +0300

    1:46:17

--- 01-react-components-finish/src/App.jsx
+++ 01-react-components-finish/src/App.jsx
@@ -27,16 +27,19 @@ export default function App() {
 					<h3>Наш подход к обучению</h3>
 
 					<ul>
+						{ways.map((way) => ( <WayToTeach key={way.title} {...way} />))}
+
 						{/* Или так, перебирая 4 штуки */}
-						<WayToTeach
+						{/* <WayToTeach
 							title={ways[0].title}
 							description={ways[0].description}
-						/>
+						/> */}
 
 						{/* или так */}
-						<WayToTeach {...ways[1]} />
+						{/* <WayToTeach {...ways[1]} />
 						<WayToTeach {...ways[2]} />
 						<WayToTeach {...ways[3]} />
+						<WayToTeach {...ways[4]} /> */}
 					</ul>
 				</section>
 				<section>
--- 01-react-components-finish/src/components/Button/Button.jsx
+++ 01-react-components-finish/src/components/Button/Button.jsx
@@ -1,8 +1,11 @@
 import './Button.css'
 
 export default function Button({ children, onClick, isActive }) {
+    let classes = 'button'
+    if (isActive) classes += ' active'
+
     return (
-        <button className={isActive ? 'button active' : 'button'} onClick={onClick}>
+        <button className={classes} onClick={onClick}>
             {children}
         </button>
 	)
