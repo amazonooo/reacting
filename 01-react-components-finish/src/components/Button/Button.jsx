ac5d46beb4e928c20572a1712e79d4882c867d43
amazonooo <garikogannisyan.9@gmail.com>
2024-03-14 16:58:48 +0300

    2:38:00

--- 01-react-components-finish/src/components/Button/Button.jsx
+++ 01-react-components-finish/src/components/Button/Button.jsx
@@ -1,11 +1,8 @@
-import './Button.css'
+import classes from './Button.module.css'
 
 export default function Button({ children, onClick, isActive }) {
-    let classes = 'button'
-    if (isActive) classes += ' active'
-
     return (
-        <button className={classes} onClick={onClick}>
+        <button className={isActive ? `${classes.button} ${classes.active}` : classes.button} onClick={onClick}>
             {children}
         </button>
 	)
