c76961fe531e168f850a4f8245d2e9646a384ce0
amazonooo <garikogannisyan.9@gmail.com>
2024-04-13 12:05:56 +0300

    опять забыл

--- email-editor/src/App.tsx
+++ email-editor/src/App.tsx
@@ -5,12 +5,11 @@ import { Details } from './Details'
 export function App() {
 
   const [title, setTitle] = useState('')
-  const [text, setText] = useState('')
 
   return (
 		<div className={styles.layout}>
 			<img src='./vite.svg' width={200} />
-			<Details title={title} text={text} setText={setText} setTitle={setTitle} />
+			<Details title={title} setTitle={setTitle} />
 		</div>
 	)
 }
