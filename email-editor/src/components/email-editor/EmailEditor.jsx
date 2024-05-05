e69b5e41d7368a57f2ab8b6acfff32e382db2a36
amazonooo <garikogannisyan.9@gmail.com>
2024-05-05 22:04:54 +0300

    hjk

--- email-editor/src/components/email-editor/EmailEditor.jsx
+++ email-editor/src/components/email-editor/EmailEditor.jsx
@@ -1,17 +1,39 @@
 import { Bold, Eraser, Italic, Underline } from 'lucide-react'
 import styles from './EmailEditor.module.scss'
+import { useRef, useState } from 'react'
 
 export function EmailEditor() {
+	const [text, setText] = useState(
+		`          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis delectus similique quod ea debitis quis dicta, qui quaerat? Vitae assumenda magni iste est, quas aperiam ea minus facilis dolorum, quibusdam vero nihil similique tempore consequuntur iusto placeat et aut voluptate eveniet? Sint est sequi nobis accusantium sed hic ea quis!`
+	)
+
+	const textRef = useRef<HTMLTextAreaElement | null>(null)
+
+	const getSelectionText = () => {
+		if(!textRef.current) return
+
+		const cursorStart = textRef.current.selectionStart;
+		const cursorEnd = textRef.current.selectionEnd;
+		const selectedText = text.substring(cursorStart, cursorEnd)
+
+		if (!selectedText) return
+	}
+
   return (
 		<div>
 			<h1>Email editor</h1>
 			<div className={styles.card}>
-				<textarea className={styles.editor} spellCheck='false'>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis delectus similique quod ea debitis quis dicta, qui quaerat? Vitae assumenda magni iste est, quas aperiam ea minus facilis dolorum, quibusdam vero nihil similique tempore consequuntur iusto placeat et aut voluptate eveniet? Sint est sequi nobis accusantium sed hic ea quis!
-				</textarea>
+				<textarea
+					ref={textRef}
+					className={styles.editor}
+					spellCheck='false'
+					onClick={getSelectionText}
+					value={text}
+					onChange={e => setText(e.target.value)}
+				/>
 				<div className={styles.actions}>
 					<div className={styles.tools}>
-						<button>
+						<button onClick={() => setText('')}>
 							<Eraser Size={17} />
 						</button>
 						<button>
