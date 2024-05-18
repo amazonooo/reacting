2571ed691170baac85ac144e2a21082534c613e4
amazonooo <garikogannisyan.9@gmail.com>
2024-05-18 20:11:05 +0300

    dsfa

---
+++ todo/src/components/layout/Layout.jsx
@@ -0,0 +1,5 @@
+const layout = ({children}) => {
+    return <div className='bg-zinc-900 h-screen py-10'>{children}</div>
+}
+ 
+export default layout;
---
+++ todo/src/components/screens/Home/Home.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import TodoItem from "./item/TodoItem";
+import CreateTodoField from "./Create-Todo-Field/CreateTodoField";
+
+const data = [
+    {
+        title: 'Finish the essay collaboration',
+        isCompleted: false,
+        _id: '123fsdaf'
+    },
+    {
+        title: 'Read next chapter of the book',
+        isCompleted: false,
+        _id: '132fssdfdaf'
+    },
+    {
+        title: 'Send the finished assignment',
+        isCompleted: false,
+        _id: '3fsdsfdaf'
+    }
+]
+
+const Home = () => {
+    const [todos, setTodos] = useState(data)
+
+    const changeTodo = id => {
+        const copy = [...todos]
+        const current = copy.find(t => t._id === id)
+        current.isCompleted = !current.isCompleted
+        setTodos(copy)
+    }
+
+    const removeTodo = id => setTodos([...todos].filter(t => t._id !== id))
+
+    return (
+			<div className='text-white w-4/5 mx-auto'>
+				<h1 className='text-2xl font-bold text-center mb-10'>
+					Todo for junior
+				</h1>
+				{todos.map(todo => (
+					<TodoItem
+						key={todo._id}
+						todo={todo}
+						changeTodo={changeTodo}
+						removeTodo={removeTodo}
+					/>
+				))}
+                <CreateTodoField setTodos={setTodos} />
+			</div>
+		)
+}
+ 
+export default Home;
