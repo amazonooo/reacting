ba470f86bfbb621c6cac3adbaf61505d6616076d
amazonooo <garikogannisyan.9@gmail.com>
2024-06-15 09:53:23 +0300

    asd

--- shop-practice/src/components/pages/home/Home.tsx
+++ shop-practice/src/components/pages/home/Home.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react'
+import styles from './Home.module.scss'
 
 const Home: FC = () => {
-    return <div>Home</div>
+    return <div className={styles.bg}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. A fugiat cupiditate odit, aspernatur voluptates, architecto dicta ut rerum praesentium optio blanditiis voluptatum totam vitae tenetur vel corporis, dignissimos officia quam itaque nemo? Reprehenderit eaque aliquam voluptate voluptatibus nesciunt maxime hic?
+    </div>
 }
 
 export default Home
--- shop-practice/src/main.tsx
+++ shop-practice/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Home from './components/pages/home/Home.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <Home />
 )
