91b5457b9016a78a8845a0aa3a1ed07ba4469ac4
amazonooo <garikogannisyan.9@gmail.com>
2024-06-20 00:35:26 +0300

    change styles for loaders

--- shop-practice/src/main.tsx
+++ shop-practice/src/main.tsx
@@ -2,24 +2,20 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Home from './components/pages/home/Home'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Product from './components/pages/Product/Product'
+import Cart from './components/pages/Cart/Cart'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <QueryClientProvider client={queryClient}>
-        <Router>
-            <Switch>
-                <Route path='/cart'>
-                    <Cart />
-                </Route>
-                <Route path='/product'>
-                    <Product />
-                </Route>
-                <Route path='/'>
-                    <Home />
-                </Route>
-            </Switch>
-        </Router>
-    </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/cart' element={<Cart />}></Route>
+				<Route path='/product/:id' element={<Product />}></Route>
+			</Routes>
+		</Router>
+	</QueryClientProvider>
 )
